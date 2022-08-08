f90aa6049f5cdd7df6d46104da7690cf92cb9be1
Socratis <Seydinamouhamadousanoumbaye@esp.sn>
2022-08-08 21:00:22 +0000

    Delete Youtube_video_downloader.py

--- Youtube_video_downloader.py
+++
@@ -1,8 +0,0 @@
-from pytube import YouTube
-from sys import argv
-link = argv[1]
-yt = YouTube(link)
-print("Title : ",yt.title)
-print("View : ",yt.views)
-yd = yt.streams.get_highest_resolution()
-yd.download("/home/socrate/Téléchargements/YoutubeDownload")
